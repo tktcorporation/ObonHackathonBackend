82583efc6e56e3b6426f581f98304da9663b4c54
tktcorporation <tktcorporation.go@gmail.com>
2021-08-15 02:34:17 +0900

    :sparkles: add messages api

--- apiapp/models.py
+++ apiapp/models.py
@@ -1,3 +1,6 @@
-# from django.db import models
+from django.db import models
+
 
 # Create your models here.
+class Message(models.Model):
+    value = models.CharField(max_length=10)
--- apiapp/views.py
+++ apiapp/views.py
@@ -1,4 +1,10 @@
-from django.http import JsonResponse
+import json
+
+from apiapp.models import Message
+from django.core import serializers
+from django.http import HttpResponse, JsonResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
 # from django.shortcuts import render
 from django.views.generic import View
@@ -7,3 +13,22 @@ from django.views.generic import View
 class TestApiView(View):
     def get(self, request):
         return JsonResponse({"status": "ok"})
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class MessagesApiView(View):
+    def get(self, request):
+        return JsonResponse(
+            {
+                "messages": [
+                    {"message": message.value} for message in Message.objects.all()
+                ]
+            }
+        )
+
+    def post(self, request):
+        body_unicode = request.body.decode("utf-8")
+        body = json.loads(body_unicode)
+        value = body["message"]
+        Message.objects.create(value=value)
+        return HttpResponse("OK")
