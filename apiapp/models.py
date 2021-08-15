0b616983e864e184932fdd22fa7f9ded480b2215
tktcorporation <tktcorporation.go@gmail.com>
2021-08-15 09:54:20 +0900

    :sparkles: Message に timestamp field を追加

--- apiapp/models.py
+++ apiapp/models.py
@@ -4,3 +4,5 @@ from django.db import models
 # Create your models here.
 class Message(models.Model):
     value = models.CharField(max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
